b50414097b66bc9897537fd5e279eecc72701da5
Nicolas Rodriguez <nrodriguez8@my.bcit.ca>
2024-10-13 20:07:04 -0700

    fixed logic not working for socketpath

--- server2/modules/dbConfig.js
+++ server2/modules/dbConfig.js
@@ -5,12 +5,11 @@ const connectionConfig = {
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'isa_project'
 };
-
 // Use the host if defined, otherwise use the socketPath if available
 if (process.env.DB_HOST) {
     connectionConfig.host = process.env.DB_HOST;
     connectionConfig.port = '3306';
-} else if (process.env.DB_SOCKET) {
+} else {
     connectionConfig.socketPath = '/run/mysqld/mysqld.sock' ;
 }
 
