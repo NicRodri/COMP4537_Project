65c71db7486d2d8007f08d26ae46eeaef8f33c12
Nicolas Rodriguez <nrodriguez8@my.bcit.ca>
2024-10-13 20:02:03 -0700

    updated so that we can run on a localhost database to test

--- server2/modules/dbConfig.js
+++ server2/modules/dbConfig.js
@@ -1,7 +1,18 @@
-module.exports = {
-    host: process.env.DB_HOST || '',
+require('dotenv').config();
+
+const connectionConfig = {
     user: process.env.DB_USER || 'Hikari',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'isa_project',
-    socketPath: process.env.DB_SOCKET || '/run/mysqld/mysqld.sock'  // Add socketPath for Unix socket
+    database: process.env.DB_NAME || 'isa_project'
 };
+
+// Use the host if defined, otherwise use the socketPath if available
+if (process.env.DB_HOST) {
+    connectionConfig.host = process.env.DB_HOST;
+    connectionConfig.port = '3306';
+} else if (process.env.DB_SOCKET) {
+    connectionConfig.socketPath = '/run/mysqld/mysqld.sock' ;
+}
+
+module.exports = connectionConfig;
+
