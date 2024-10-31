f230448a42941e480a3657485af4c31ab232cf66
Nicolas Rodriguez <nrodriguez8@my.bcit.ca>
2024-10-30 22:40:44 -0700

    added console.logs to test

--- server2/modules/connectML.js
+++ server2/modules/connectML.js
@@ -3,12 +3,16 @@ async function connectML(imageBuffer) {
     const { Blob } = require('buffer');
     const exampleImage = new Blob([imageBuffer], { type: 'image/png' });
 
+
     const client = await Client.connect("https://homura.makeup/");
-    const result = await client.predict("/predict", {
+    console.log(client)
+    console.log(exampleImage)
+    const result = await client.predict("/gradio_api/predict", {
         image: exampleImage,
         source_age: 21,
         target_age: 80,
     });
+    console.log(result.data);
 
     return result;
 }
